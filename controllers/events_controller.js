bd8ef4e871ac95f99dd8a4b93e04ffa71b82d97f
tjs6595 <t.schultejans@schenckprocess.com>
2022-08-07 14:51:30 -0500

    Commit before final check.

---
+++ controllers/events_controller.js
@@ -0,0 +1,83 @@
+// DEPENDENCIES
+const events = require('express').Router()
+const db = require('../models')
+//const band = require('../models/event')
+const { Event } = db 
+
+// FIND ALL EVENTS (INDEX ROUTE)
+events.get('/', async (req, res) => {
+    try {
+        const foundEvents = await Event.findAll({
+            order: [['event_id', 'ASC']],
+            where: {
+                name: {[OP.like]: `%${req.query.name ? req.query.name : ''}%`}
+            }
+        })
+        res.status(200).json(foundEvents)
+    }
+    catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// FIND ONE EVENT (SHOW ROUTE)
+events.get('/:id', async (req, res) => {
+    try {
+        const foundEvent = await Event.findOne({
+            where: { event_id: req.params.id }
+        })
+        res.status(200).json(foundEvent)
+    } 
+    catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// CREATE AN EVENT
+events.post('/', async (req, res) => {
+    try {
+        const newEvent = await Event.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new event.',
+            data: newEvent
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// UPDATE AN EVENT
+events.put('/:id', async (req, res) => {
+    try {
+        const updatedEvents = await Event.update(req.body,{
+            where: {
+                event_id: req.params.id
+                }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedEvents} event(s).`
+        })
+    } 
+    catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE A BAND
+events.delete('/:id', async (req, res) => {
+    try {
+        const deletedEvents = await Event.destroy({
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedEvents} event(s).`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// EXPORT
+module.exports = events;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 
 // ROOT
 app.get('/', (req, res) => {
@@ -13,6 +16,18 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+
+
+// CONTROLLERS 
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
+
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
+
 
 // LISTEN
 app.listen(process.env.PORT, () => {
